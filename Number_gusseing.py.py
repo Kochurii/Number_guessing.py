9544aecd642eafe8192e13f5e288f6381f01c0b9
shayani Banerjee <138789511+Kochurii@users.noreply.github.com>
2024-08-17 14:38:48 +0530

    Add files via upload

---
+++ Number_gusseing.py.py
@@ -0,0 +1,18 @@
+
+secret_number = "7"
+guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = False
+
+while guess != secret_number and not(out_of_guesses):
+     if guess_count < guess_limit:
+          guess = input("Enter a guess: ")
+          guess_count += 1
+     else:
+          out_of_guesses = True
+
+if out_of_guesses:
+     print("You Lose!")
+else:
+     print("You Win!")
